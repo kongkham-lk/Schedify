68d95ea9b54d6bd58e4ab35c1fd9371be541ffe4
Reuben <reubenpainter@gmail.com>
2024-11-07 13:59:31 -0800

    Main Page of the project
    The 4 fragments with a bottom navigation that navigates to each fragment, and a button in the create fragment traverses to an activity. The activity also has a button that returns back to that fragment.

---
+++ app/src/main/java/com/example/schedify/CreateTaskActivity.java
@@ -0,0 +1,41 @@
+package com.example.schedify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        setContentView(R.layout.activity_create_task);
+
+        Button returnBtn = findViewById(R.id.return_btn);
+
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("FragmentToLoad", "CreateFragment");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/schedify/MainActivity.java
+++ app/src/main/java/com/example/schedify/MainActivity.java
@@ -1,20 +1,84 @@
 package com.example.schedify;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                if (item.getItemId() == R.id.nav_home) {
+                    fragment = new com.example.schedify.HomeFragment();
+                } else if (item.getItemId() == R.id.nav_create) {
+                    fragment = new com.example.schedify.CreateFragment();
+                } else if (item.getItemId() == R.id.nav_profile) {
+                    fragment = new ProfileFragment();
+                } else if (item.getItemId() == R.id.nav_settings) {
+                    fragment = new SettingsFragment();
+                }
+
+                if (fragment != null) {
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frameLayout, fragment);
+                    fragmentTransaction.commit();
+                }
+
+                return true;
+            }
+        });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frameLayout, new com.example.schedify.HomeFragment())
+                    .commit();
+        }
+
+        String fragmentToLoad = getIntent().getStringExtra("FragmentToLoad");
+        if (fragmentToLoad != null) {
+            Fragment fragment = null;
+            if ("CreateFragment".equals(fragmentToLoad)) {
+                fragment = new com.example.schedify.CreateFragment();
+                bottomNavigationView.setSelectedItemId(R.id.nav_create);
+            } else if ("CreateFragment2".equals(fragmentToLoad)) {
+                fragment = new com.example.schedify.CreateFragment();
+                bottomNavigationView.setSelectedItemId(R.id.nav_create);
+            }
+            if (fragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frameLayout, fragment)
+                        .commit();
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
